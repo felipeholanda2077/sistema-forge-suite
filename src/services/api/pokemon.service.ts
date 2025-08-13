8d4be7fc3756fa8a854254f4b1805493ff1418b0
felipeholanda2077 <felipe_holandafreitas@hotmail.com>
2025-08-13 14:35:23 -0300

    fix: change to production url

--- src/services/api/pokemon.service.ts
+++ src/services/api/pokemon.service.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL =  'https://back-end-projetokirvano-b30617960bd0.herokuapp.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -1,6 +1,6 @@
 import { Pokemon } from './pokemonApi';
 
-const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:3002/api';
+const API_BASE_URL = 'https://back-end-projetokirvano-b30617960bd0.herokuapp.com/api';
 
 interface ApiErrorResponse {
   message?: string;
